4f2a7e6a95984e07d48123d7cf3db9e87cbdf3ee
kantakaneda <k.kaneda@keio.jp>
2022-04-28 14:12:32 +0900

    [CHANGE] 2 pass hashの手法を追加

--- src/001.py
+++ src/001.py
@@ -8,13 +8,25 @@ You can return the answer in any order.
 from typing import List
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum_brute(self, nums: List[int], target: int) -> List[int]:
+        """ 力づく """
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
 
+    def twoSum_2hash(self, nums: List[int], target: int) -> List[int]:
+        """ 2 pass hash を用いた手法 """
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
+
 if __name__ == "__main__":
     x = Solution()
-    r = x.twoSum([2,7,11,15], 9)
+    r = x.twoSum_brute([2,7,11,15], 9)
+    r = x.twoSum_2hash([2,7,11,15], 9)
     print(r)
