11de0150009f7b17c237fda1687878f1b8f05051
kantakaneda <k.kaneda@keio.jp>
2022-04-28 14:04:41 +0900

    [ADD] Q1. Two Sumのコードを追加

---
+++ src/001.py
@@ -0,0 +1,20 @@
+
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+"""
+
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+if __name__ == "__main__":
+    x = Solution()
+    r = x.twoSum([2,7,11,15], 9)
+    print(r)
